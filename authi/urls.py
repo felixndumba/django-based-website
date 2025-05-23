007587d8c819adbc29087e0643adceabd937bb90
f3lo <bundif2003@gmail>
2025-05-23 17:46:53 +0300

    initial commit

---
+++ authi/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_view
+from django.contrib import admin
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', views.custom_logout, name="logout"),
+    path('stores/',views.findstore,name="findstore"),
+    path('featured/',views.featuredproducts,name="fproducts"),
+    path('payment/',views.paymentmethod,name= "payment")
+]
---
+++ authi/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+
+def home(request):
+    return render(request, 'users/home.html')
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Hi {username}, your account was created successfully')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'users/register.html', {'form': form})
+
+
+@login_required()
+def profile(request):
+    return render(request, 'users/profile.html')
+
+@login_required  # Ensures only logged-in users can access this
+def custom_logout(request):
+    logout(request)  # Django's logout function
+    return render(request, 'users/login.html') 
+
+@login_required()
+def findstore(request):
+    return render(request, 'users/store.html')
+
+@login_required()
+def featuredproducts(request):
+    return render(request,'users/fproducts.html')
+
+def paymentmethod(request):
+    return render(request,'users/payment.html')
+
